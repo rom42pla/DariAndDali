c2f9b63b9c12c46af8a8e741ac623aab18d5dfd5
AlessioGennari <genn.alessio@gmail.com>
2019-06-23 12:10:17 +0200

    Creato il menu in cui ci si può navigare  -creato un prefab per la UI in game. Alla creazione di un nuovo livello basta aggiungere quel prefab per avere la UI gia pronta. L'unica cosa da fare è nel GameObject restart, nell'evento onClick() cambiare il nome della scene.

---
+++ Assets/Scripts/UI/ButtonHandler.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+
+    public GameObject soundOn;
+    public GameObject soundOff;
+    public void Exit()
+    {
+        Application.Quit();
+        print("uscito");
+    }
+
+
+    public void ToggleSound()
+    {
+
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            PlayerPrefs.SetInt("Muted", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+
+        }
+
+        SetSoundState();
+
+    }
+
+    private void SetSoundState()
+    {
+
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            AudioListener.volume = 1;
+            soundOn.SetActive(true);
+            soundOff.SetActive(false);
+            print("volume: " + AudioListener.volume);
+            print(PlayerPrefs.GetInt("Muted", 0));
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            soundOn.SetActive(false);
+            soundOff.SetActive(true);
+            print("volume: " + AudioListener.volume);
+            print(PlayerPrefs.GetInt("Muted", 0));
+        }
+
+    }
+
+    public void Pause(GameObject PauseUI)
+    {
+        PauseUI.SetActive(true);
+        //transform.parent.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+        print(transform.parent.gameObject.name);
+        print(Time.timeScale);
+        
+    }
+    public void Resume() {
+        transform.parent.gameObject.SetActive(false);
+       
+        Time.timeScale = 1f;
+    }
+
+    public void Reverse(GameObject ui) {
+        Vector3 coord = new Vector3(0, 180, 180);
+        ui.transform.Rotate(coord);
+    }
+
+}
